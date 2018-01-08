7129aebb19cf7495a80e55b49014246de9a1c545
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-08 15:10:30 +0000

    Create characterize.tcl

---
+++ scr/characterize.tcl
@@ -0,0 +1,31 @@
+# Characterize and write_script for all modules 
+current_design ChipLevel
+characterize u1
+current_design Adder16
+write_script > "${script_path}adder16.wtcl"
+current_design ChipLevel
+characterize u2
+current_design CascadeMod
+write_script -format tcl"${script_path}cascademod.wtcl"
+current_design ChipLevel
+characterize u3
+current_design Comparator
+write_script -format tcl > "${script_path}comp16.wtcl"
+current_design ChipLevel
+characterize u4
+current_design Multiply8x8
+write_script -format tcl > "${script_path}mult8.wtcl"
+current_design ChipLevel
+characterize u5
+current_design Multiply16x16
+write_script -format tcl > "${script_path}mult16.wtcl"
+current_design ChipLevel
+characterize u6
+current_design MuxMod
+write_script -format tcl > "${script_path}muxmod.wtcl"
+current_design ChipLevel
+characterize u7
+current_design PathSegment
+echo "current_design PathSegment" > "${script_path}pathseg.wtcl"
+echo "group -design mult -cell mult U100" >> "${script_path}pathseg.wtcl"
+write_script -format tcl >> "${script_path}pathseg.wtcl"
