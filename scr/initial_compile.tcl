b6012d970e72ac1f2c84999bc90d33679ff45c95
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-08 14:53:40 +0000

    Create initial_compile.tcl

---
+++ scr/initial_compile.tcl
@@ -0,0 +1,11 @@
+# Initial compile with estimated constraints 
+source "${script_path}read.tcl"
+current_design ChipLevel
+source "${script_path}defaults.tcl"
+source "${script_path}adder16.tcl"
+source "${script_path}cascademod.tcl"
+source "${script_path}comp16.tcl"
+source "${script_path}mult8.tcl"
+source "${script_path}mult16.tcl"
+source "${script_path}muxmod.tcl"
+source "${script_path}pathseg.tcl"
